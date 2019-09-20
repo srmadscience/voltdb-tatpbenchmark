58247c77a2f3b49f13664adb999bc4924d121582
David Rolfe <drolfe@voltdb.com>
2019-09-20 09:52:40 +0100

    fix basedir

--- scripts/runbenchmark.sh
+++ scripts/runbenchmark.sh
@@ -38,7 +38,7 @@ for i in  0 1 2
 do
         java -Dlog4j.configurationFile=src/main/java/org/voltdb/tatp/client/log4j2-test.xml -jar target/voltdb-tatpbenchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar  \
                 ${TNAME} ${HNAME} ${STARTTPS} ${INCTPS} ${SUBS} ${i} ${MINS} ${THREADS} |  \
-                tee -a  results/${TNAME}_${HNAME}_${STARTTPS}_${INCTPS}_${SUBS}_${i}_${MINS}_${THREADS}.log
+                tee -a  ${BASEDIR}/../results/${TNAME}_${HNAME}_${STARTTPS}_${INCTPS}_${SUBS}_${i}_${MINS}_${THREADS}.log
 
 done
 
