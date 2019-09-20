c12ac72c088d51fad9af13996ce447e2102c2c8a
David Rolfe <drolfe@voltdb.com>
2019-09-20 09:57:50 +0100

    fix basedir

--- scripts/runbenchmark.sh
+++ scripts/runbenchmark.sh
@@ -34,12 +34,18 @@ SUBS=5000000
 MINS=5
 THREADS=10
 
+if
+	[ ! -d results ]
+then
+	mkdir results
+fi
+
 # We test three ways of solving the 'foreign key' problem
 for i in  0 1 2
 do
         java -Dlog4j.configurationFile=src/main/java/org/voltdb/tatp/client/log4j2-test.xml -jar target/voltdb-tatpbenchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar  \
                 ${TNAME} ${HNAME} ${STARTTPS} ${INCTPS} ${SUBS} ${i} ${MINS} ${THREADS} |  \
-                tee -a  ${BASEDIR}/../results/${TNAME}_${HNAME}_${STARTTPS}_${INCTPS}_${SUBS}_${i}_${MINS}_${THREADS}.log
+                tee -a  results/${TNAME}_${HNAME}_${STARTTPS}_${INCTPS}_${SUBS}_${i}_${MINS}_${THREADS}.log
 
 done
 
